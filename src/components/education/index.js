dfc39c734586247931e01b7a5dd0c0787d2beb89
vichi06 <vincent.n@hotmail.fr>
2024-09-10 11:43:15 +0200

    [Education] redesigned

--- src/components/education/index.js
+++ src/components/education/index.js
@@ -5,6 +5,8 @@ import "./index.css";
 import civ from "../../assets/logo-CIV.jpg";
 import descartes from "../../assets/descartes.jpg";
 import efrei from "../../assets/efrei.jpg";
+import { GiDiploma } from "react-icons/gi";
+import { BsStars } from "react-icons/bs";
 
 const education = [
   {
@@ -12,16 +14,18 @@ const education = [
     finishingYear: 2015,
     name: "Collège Internationnal de Valbonne",
     city: "Sophia-Antipolis",
-    diploma: "Brevet des Collèges",
+    diploma: "Brevet des Collèges - mention très bien",
     logo: civ,
+    details: [],
   },
   {
     beginningYear: 2015,
     finishingYear: 2018,
     name: "Lycée Internationnal de Valbonne",
     city: "Sophia-Antipolis",
-    diploma: "Baccalauréat",
+    diploma: "International Scientific Baccalauréat - mention bien",
     logo: civ,
+    details: [],
   },
   {
     beginningYear: 2018,
@@ -30,65 +34,58 @@ const education = [
     city: "Paris 16",
     diploma: "DUT Informatique",
     logo: descartes,
+    details: [
+      "End of studies intership with a researcher of the ENS working on cognitive sciences. Development of a web experience (spaceship game) to collect sensitivity results from patients.",
+    ],
   },
   {
     beginningYear: 2020,
     finishingYear: 2023,
     name: "EFREI Paris",
     city: "Villejuif",
-    diploma: "Diplôme d'ingénieur",
+    diploma: "Engineer Diploma, CTI Certified",
     logo: efrei,
+    details: [
+      "From October 2023 - March 2024, University exchange with the Nagoya Institute of Technology in Japan for a 6 months research on Artificial Intelligence.",
+    ],
   },
 ];
 
 function Education() {
-  const [periodChosen, setPeriodChosen] = useState(0);
-
   return (
-    <table className="Education">
-      <thead className="Education-header">
-        <tr>
-          <th scope="col">Années</th>
-          <th scope="col">Nom de l'institut</th>
-          <th scope="col">Diplôme</th>
-        </tr>
-      </thead>
-      <tbody>
-        {education.map((value, index) => (
-          <tr
-            className={index === periodChosen ? "selected" : ""}
-            onMouseOver={() => setPeriodChosen(index)}
-          >
-            <th scope="row">
-              {value.beginningYear} - {value.finishingYear}
-            </th>
-            <td
-              style={{
-                display: "flex",
-                justifyContent: "end",
-                gap: "10px",
-                alignItems: "center",
-              }}
-            >
-              <div>
-                {value.name}
-                <br />
-                <i>{value.city}</i>
+    <div className="Education">
+      {education.map((e) => (
+        <div className="Education-Section">
+          <div className="Education-Vertical-Line">
+            <div />
+          </div>
+          <div>
+            <div className="Education-Circle">
+              <div />
+            </div>
+            <h2>{e.name}</h2>
+          </div>
+          <div>
+            <p>
+              {e.city}, {e.beginningYear} - {e.finishingYear}
+            </p>
+          </div>
+          <div style={{ gap: 10 }}>
+            <GiDiploma size={30} />
+            <p>{e.diploma}</p>
+          </div>
+          {e.details.map((d) => (
+            <div>
+              <div className="Education-Circle" style={{ padding: "5px 0" }}>
+                <div style={{ width: "80%", height: "80%" }} />
+                <BsStars style={{ position: "absolute" }} color="white" size={10} />
               </div>
-              <div>
-                <img
-                  src={value.logo}
-                  alt="logo"
-                  style={{ height: 50, width: 50, objectFit: "contain" }}
-                />
-              </div>
-            </td>
-            <td>{value.diploma}</td>
-            <div></div>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              <p style={{ width: "fit-content" }}>{d}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
 
